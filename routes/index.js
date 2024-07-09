7db7d4fa818351e94f9b8cb9efca8bb739c087f8
Joaquin Bonaudi <jtbmelgar@gmail.com>
2024-07-09 14:03:30 +0200

    solved

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const Movie = require("../models/movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies", { movies: movies });
+    })
+    .catch((error) => {
+      console.error("Error connecting to the database", error);
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      res.render("movie", { movie: movie });
+    })
+    .catch((error) => {
+      console.error("Error connecting to the database", error);
+    });
+});
 
 module.exports = router;
