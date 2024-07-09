c9321f7c66594a20b478851d3131e933a3193abb
Joaquin Bonaudi <jtbmelgar@gmail.com>
2024-07-09 13:59:03 +0200

    solved

---
+++ models/movie.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    cast: {
+        type: [String],
+        required: true
+    },
+    plot: {
+        type: String,
+        required: true
+    }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
